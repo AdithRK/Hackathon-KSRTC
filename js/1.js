a528f0cbdfcc324e9dcc531792d7cfc9a12048ab
Adith R K <adith3999@gmail.com>
2022-01-29 21:09:59 +0530

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,14 @@
+function showcapital ()
+    		{
+    			var cc,ans;
+    			c=document.frmstate.state.selectedIndex
+    			switch(C)
+    			{
+    			case 0: ans="TVM";break;
+    			case 1: ans="chennai";break;
+    			case 2: ans="bangalore";break;
+    			case 3: ans="hyderabad";break;
+    			case 4: ans="shimla";break;
+    			case 5: ans="mumbai";break;
+    			}document.frmstate.txtcapital.value=ans;
+    		}
